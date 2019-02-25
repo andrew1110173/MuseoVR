8ff135334cadf6f0d8fe09667bcb6ae54fd37db9
HelloImZomi <zomizx@hotmail.com>
2019-02-24 21:30:31 -0700

    Dialogo con música
    Escribe el texto letra por letra y reproduce un audio al mismo tiempo

---
+++ MuseoVR/MuseoVR/Assets/Scripts/Dialog.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class Dialog : MonoBehaviour {
+
+    [SerializeField]
+    string Title;
+    [SerializeField]
+    string Description;
+    [SerializeField]
+    string Character;
+
+    [SerializeField]
+    int Year;
+
+    [SerializeField]
+    GameObject DialogBox;
+
+    //Texto del canvas
+    private Text Title_Edit;
+    private Text Description_Edit;
+    private Text Character_Edit;
+
+    //Escritura de texto
+    public float Delay = 0.1f;
+    private string currentText = "";
+    Coroutine lastRoutine = null;
+
+    //Reproduccion de audio
+    private AudioSource audioData;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DialogBox.transform.Find("Title").GetComponent<Text>().text = Title;
+        DialogBox.transform.Find("Character").GetComponent<Text>().text = Character;
+
+        if (other.tag == "Hand")
+        {
+            DialogBox.SetActive(true);
+            lastRoutine = StartCoroutine(DrawText());
+            audioData.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        audioData.Stop();
+        HideDialogBox();
+        StopCoroutine(lastRoutine);
+        currentText = "";
+        DialogBox.transform.Find("Description").GetComponent<Text>().text = "";
+    }
+
+    IEnumerator DrawText()
+    {
+        for (int i = 0; i < Description.Length; i++)
+        {
+            currentText = Description.Substring(0, i);
+            DialogBox.transform.Find("Description").GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(Delay);         
+        }
+    }
+
+
+    void HideDialogBox()
+    {
+        DialogBox.SetActive(false);
+    }
+
+}
---
+++ MuseoVR/MuseoVR/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using  Core.VRMovementSystem;
+
+class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    float moveSpeed;
+
+    [SerializeField]
+    float rotSpeed;
+
+    void Update()
+    {
+        VRMovementSystem.VRMovement(transform, moveSpeed, rotSpeed);
+    }
+}
+
